3638a980e8ae323b67ca53198c3a36289c94b6c6
SaloniMishra <83867441+SaloniMishra@users.noreply.github.com>
2021-06-21 18:59:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+
+window.onload = function setTemplate() {
+    localStorage.clear();
+    document.getElementById('allComments').innerHTML = localStorage.getItem('template');
+};
+
+const commentContainer = document.getElementById('allComments');
+document.getElementById('addComments').addEventListener('click', function (ev) {
+   addComment(ev);
+});
+
+function addComment(ev) {
+    let commentText, wrapDiv;
+    const textBox = document.createElement('div');
+    const replyButton = document.createElement('button');
+    replyButton.className = 'reply';
+    replyButton.innerHTML = 'Reply';
+    const likeButton = document.createElement('button');
+    likeButton.innerHTML = 'Like';
+    likeButton.className = 'likeComment';
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+    deleteButton.className = 'deleteComment';
+    if(hasClass(ev.target.parentElement, 'container')) {
+        const wrapDiv = document.createElement('div');
+        wrapDiv.className = 'wrapper';
+        wrapDiv.style.marginLeft = 0;
+        commentText = document.getElementById('comment').value;
+        document.getElementById('comment').value = '';
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "cornflowerblue";
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+        commentContainer.appendChild(wrapDiv);
+    } else {
+        wrapDiv = ev.target.parentElement;
+        commentText = ev.target.parentElement.firstElementChild.value;
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "paleturquoise";
+        wrapDiv.innerHTML = '';
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+    }
+    setOnLocalStorage();
+}
+
+function setOnLocalStorage () {
+    localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+}
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
+document.getElementById('allComments').addEventListener('click', function (e) {
+    if (hasClass(e.target, 'reply')) {
+        const parentDiv = e.target.parentElement;
+        const wrapDiv = document.createElement('div');
+        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+        wrapDiv.className = 'wrapper';
+        const textArea = document.createElement('textarea');
+        textArea.style.marginRight = '20px';
+        const addButton = document.createElement('button');
+        addButton.className = 'addReply';
+        addButton.innerHTML = 'Add';
+        const cancelButton = document.createElement('button');
+        cancelButton.innerHTML = 'Cancel';
+        cancelButton.className='cancelReply';
+        wrapDiv.append(textArea, addButton, cancelButton);
+        parentDiv.appendChild(wrapDiv);
+
+    } else if(hasClass(e.target, 'addReply')) {
+        addComment(e);
+    } else if(hasClass(e.target, 'likeComment')) {
+         const likeBtnValue = e.target.innerHTML;
+         e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'cancelReply')) {
+        e.target.parentElement.innerHTML = '';
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'deleteComment')) {
+        e.target.parentElement.remove();
+    }
+});
+
+
